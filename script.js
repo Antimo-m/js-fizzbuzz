50ebe7f136759d6484d171ea964c5e3f7bf8283a
Antimo-M <montellaantimo@live.it>
2025-10-28 15:08:21 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+//dobbiamo scrivere un programma che stampi i numeri da 1 a 100.
+//per i multipli di 3 stampiamo il termine fizz piuttosto che il numero 
+//per i mutlipli di 5 invece stampiamo il termine buzz piuttosto che il numero
+//per invece i numeri che sono multipli sia di 3 che di 5 stampremo la scritta fizzbuzz
+
